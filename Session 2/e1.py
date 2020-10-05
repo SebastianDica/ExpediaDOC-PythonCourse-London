0ffbadec9eb6409c984914b33effcdba7a009c65
Sebastian Dica <fdica@LONC02TNCXMG8WN.sea.corp.expecn.com>
2020-10-05 09:56:01 +0100

    Adding S2E1

---
+++ Session 2/e1.py
@@ -0,0 +1,19 @@
+x = {}
+
+x['property1'] = 2
+
+print(x['property1']) # What would happen here?
+
+# print(x['property2']) # What would happen here?
+
+# x['property2'] = "hello"
+# print(x['property2']) # What would happen here?
+#
+# x['property3'] = ["hello", "world"]
+# print(x['property3']) # What would happen here?
+#
+# x['property4'] = {"property1": 10}
+# print(x['property4']) # What would happen here?
+#
+# x['property5'] = x
+# print(x['property5']) # What would happen here?
